92804ceb3f8093084d22ba1da79eecfdca612e60
Vaish-B <vaishnavibadve1998@gmail.com>
2022-04-19 17:00:26 +0530

    added

---
+++ vaishnavi/Classloader.java
@@ -0,0 +1,19 @@
+package javapractice;
+
+import java.io.ObjectInputStream.GetField;
+
+public class Classloader {
+	
+	private static final char[] GetClassloader = null;
+
+	public static void main(String args[]){
+	 
+
+	Class c =  Classloader.class;
+	
+	 
+
+	System.out.println(GetClassloader);
+	 
+	}
+}

86a5362eb5924831a5355bd95fa4d6bd92624861
sinugaud <sinugaud@gmail.com>
2022-04-19 12:58:51 +0530

    ClassLoader Exmple

---
+++ shailendra/Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+    public static void main(String args[])
+    {
+        Class A = Hello.class;
+        System.out.println(Hello.class);
+
+        System.out.println(String.class.getClassLoader());
+    }
+    }

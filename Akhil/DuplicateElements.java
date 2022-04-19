09da528fc6fd9de3274d1e837873f0a3f13243c8
Akhil <akhilsiddabattula@gmail.com>
2022-04-19 18:11:14 +0530

    Done

---
+++ Akhil/DuplicateElements.java
@@ -0,0 +1,19 @@
+package training;
+
+public class DuplicateElements {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 1, 2, 3, 4, 4, 3, 5, 5};
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					System.out.println(arr[i]);
+				}
+			}
+		}
+
+	}
+
+}

eb50da9e84979240bf6ad7efcff69c12dd80ddb9
Vaish-B <96465233+Vaish-B@users.noreply.github.com>
2022-04-20 20:48:26 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package project1;
+
+
+class Student{
+String name;
+int roll_no;
+}
+class test{
+public static void main(String[] args){
+Student s=new Student();
+s.name="john";
+s.roll_no=2;
+System.out.println("s.na and s.roll_no  +s.name,s.roll_no");
+}
+}
+

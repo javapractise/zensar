687fb24efee79d937427310158e2953729106001
sinugaud <sinugaud@gmail.com>
2022-04-20 10:10:28 +0530

    added other program

--- shailendra/AssendingOrder.java
+++ shailendra/AssendingOrder.java
@@ -3,8 +3,27 @@ package com.java.practice;
 public class AssendingOrder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// Initialize array
+		int[] arr = { 4, 5, 62, 1, 2, 7, 6, 9 };
+		int temprory = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					temprory = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temprory;
+				}
+			}
+		}
+
+		System.out.println();
+
+	
+		System.out.println("Elements of array sorted in ascending order: ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
-
 }

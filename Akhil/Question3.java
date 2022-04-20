03e4a97a7eceb3fe812bf04da93bf9404299430b
Akhil <akhilsiddabattula@gmail.com>
2022-04-20 17:45:09 +0530

    Done

---
+++ Akhil/Question3.java
@@ -0,0 +1,52 @@
+package training2;
+
+class Member {
+	String name, address;
+	int age, phoneNumber, salary;
+
+	public void printSalary() {
+		System.out.print(salary);
+	}
+
+}
+
+class Manager extends Member {
+	String specilization, department;
+	
+	public void printSalary(){
+		System.out.println(department);
+	}
+}
+
+class Employee extends Member {
+	String specilization, department;
+}
+
+public class Question3 {
+
+	public static void main(String[] args) {
+
+		Employee e1 = new Employee();
+
+		e1.name = "Akhil Siddabattula";
+		e1.age = 22;
+		e1.phoneNumber = 70100000;
+		e1.salary = 30000;
+		e1.address = "Andhra Pradesh";
+		e1.department = "Developing";
+		e1.specilization = "Java Developer";
+		
+		e1.printSalary();
+		
+		Manager m1 = new Manager();
+		e1.name = "Akhil Siddabattula";
+		e1.age = 22;
+		e1.phoneNumber = 70100000;
+		e1.salary = 100000;
+		e1.address = "Andhra Pradesh";
+		e1.department = "Developing";
+		e1.specilization = "sdsdssf";
+		
+		m1.printSalary();
+	}
+}

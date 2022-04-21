32e559ac8d86a3a80fb6dd5096fc7daaccc2e243
shubhammahale83 <shubhammahale83@gmail.com>
2022-04-21 06:01:37 -0700

    ShubhJava

---
+++ ShubhPassenger/TaxiPassenger.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class TaxiPassenger {
+
+	public static void main(String[] args) {
+		
+		int total = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+
+		int[][] a = new int[m+1][n+1];
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int k=0; k<a.length; k++) {
+			for(int l=0; l<a[k].length; l++) {
+				if(a[k][l] != -1) {
+					total = total + a[k][l];
+				}
+			}
+		}
+		
+		System.out.println("These are the Total Passengers traveled from Taxi: "+total);
+		
+		}
+}

fbf7a8dbcc0ba73e58d091c9170eaf3f1f41bfd4
Akhil <akhilsiddabattula@gmail.com>
2022-04-21 12:03:02 +0530

    Done

---
+++ Akhil/ExtendingInterfaces.java
@@ -0,0 +1,32 @@
+package training4;
+
+interface Vehicle {
+	
+	public void getPrint();
+}
+
+interface Car extends Vehicle {
+	
+	public void getCarName(String name);
+}
+
+
+public class ExtendingInterfaces implements Car {
+	
+	public void getPrint() {
+		System.out.println("This is vehicle interface");
+	}
+	
+	public void getCarName(String name) {
+		System.out.println("This is a " + name + " car");
+	}
+	
+	public static void main(String[] args) {
+		
+		ExtendingInterfaces e = new ExtendingInterfaces();
+		
+		e.getPrint();
+		e.getCarName("Benz");
+	}
+
+}

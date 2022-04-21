b3e519742d37fd2ffa435fe2ab3aa59a74b0b6cd
hemanth <h.mabbu@zensar.com>
2022-04-21 08:40:49 +0530

    inheritance programs

---
+++ hemanth/master.java
@@ -0,0 +1,15 @@
+class Animal{  
+void eat(){System.out.println("eating...");}  
+}  
+class dog extends Animal{  
+void bark(){System.out.println("barking...");}  
+}  
+class Cat extends Animal{  
+void meow(){System.out.println("meowing...");}  
+}  
+class TestInheritance3{  
+public static void main(String args[]){  
+Cat c=new Cat();  
+c.meow();  
+c.eat(); 
+}

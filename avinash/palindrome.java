23f67d4bf7b15990e945bb5260fd37a8a63ce93a
Avinash Reddy Yasa <a.yasa@zensar.com>
2022-04-22 12:09:28 +0530

    test2

---
+++ avinash/palindrome.java
@@ -0,0 +1,24 @@
+class palindrome
+{
+public static void  main(String args[])
+{
+int no=121;
+int temp=no;
+int rev=0,rem;
+while(temp !=0)
+{
+rem=temp%10;
+rev=rev*10+rem;
+temp=temp/10;
+}
+if(no==rev)
+{
+System.out.println(no+"is palindrome number");
+}
+else
+{
+System.out.println(no+"is not palindrome number");
+}
+}
+}
+

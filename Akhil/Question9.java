e870143384629328746d58f579b787264b5edb59
Akhil <akhilsiddabattula@gmail.com>
2022-04-22 13:57:45 +0530

    Done

---
+++ Akhil/Question9.java
@@ -0,0 +1,16 @@
+package training6;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		
+		String str = "This is java microservies batch";
+		char ch = '-';
+		
+		str = str.replace(' ', ch);
+		
+		System.out.println(str);
+
+	}
+
+}

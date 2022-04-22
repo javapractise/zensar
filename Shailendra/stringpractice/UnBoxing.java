c95f6d7b3d2497d712e4d3b499dfe93a90e27d2b
sinugaud <sinugaud@gmail.com>
2022-04-22 14:00:58 +0530

    String Assignment

---
+++ Shailendra/stringpractice/UnBoxing.java
@@ -0,0 +1,11 @@
+package com.java.stringpractice;
+
+public class UnBoxing {
+
+	public static void main(String[] args) {
+	Integer integer =new Integer(45);
+	int i =integer;
+	System.out.println(i);
+	}
+
+}

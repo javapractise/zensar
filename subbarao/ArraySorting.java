67fb2728cea8fdda007cc0f9df96a40ae2f870d8
Subbarao Kalluri <s.kalluri@zensar.com>
2022-04-22 18:10:02 +0530

    test3

---
+++ subbarao/ArraySorting.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class ArraySorting {
+
+	public static void main(String[] args) {
+
+		int[] arr = {2, 4, 1, 3, 5};
+
+		System.out.println("Original Array:");
+		for(int num: arr) {
+			System.out.print(num + " ");
+		}
+
+		Arrays.sort(arr);
+		System.out.println();
+
+		System.out.println("Sorted Array:");
+		for(int num: arr) {
+			System.out.print(num + " ");
+		}
+
+	}
+
+}

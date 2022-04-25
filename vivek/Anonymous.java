dd693030d4f4328529a886720cf94dd02771c11e
Vivek Ambetkar <v.ambetkar@zensar.com>
2022-04-25 04:54:10 -0700

    Done

---
+++ vivek/Anonymous.java
@@ -0,0 +1,13 @@
+public class Demo {
+	public void example() {
+		System.out.println("Anonymous Inner Class" );
+	}
+	}
+ class Anonymous {
+	public static void main(String[] args) {
+		Demo d1=new Demo();
+		d1.example();
+
+	}
+
+}

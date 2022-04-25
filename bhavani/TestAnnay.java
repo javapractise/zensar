93a6834b8e06a61fc9a1e27452fb9510010bf1e5
bhavani <abc@gmail.com>
2022-04-25 16:56:50 +0530

    done

---
+++ bhavani/TestAnnay.java
@@ -0,0 +1,16 @@
+interface Game
+{
+void paly();
+class TestAnnoy
+{
+public static void main(String args[])
+{
+Game g= new Game();
+{
+public void play()
+{
+System.out.println("playing game");
+}
+};
+g.play();
+}

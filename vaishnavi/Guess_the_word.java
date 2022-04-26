abb263a440b432011f6c26938bda29b03be05e56
Vaish-B <96465233+Vaish-B@users.noreply.github.com>
2022-04-26 15:01:53 +0530

    vaishnavi_codingTest_output
    added

---
+++ vaishnavi/Guess_the_word.java
@@ -0,0 +1,52 @@
+package project1;
+import java.util.Scanner;
+
+public class Guess_the_word {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		String[] str = new String[n];
+		String res = "";
+
+		for (int i = 0; i < n; i++) {
+			str[i] = scan.next();
+			if (str[i].length() % 2 != 0 && str[i].length() > res.length()) {
+				res = str[i];
+			}
+		}
+
+		if (res.length() % 2 != 0) {
+			System.out.println(res);
+		} else {
+			System.out.println("Better luck next time");
+		}
+	}
+
+}
+
+/*
+*
+--Input & Output--
+5
+Hello
+Good
+Morning
+Welcome
+You
+output :Morning
+-----------------------
+3
+Go
+To
+Hell
+output :Better Luck Next Time..!
+-----------------------
+4
+My
+Name
+Is
+vaishnavi
+output :vaishnavi
+* 
+* */

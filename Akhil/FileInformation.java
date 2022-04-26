3d2a8a2f22f24953969d4fc8c7f27023074136fd
Akhil <akhilsiddabattula@gmail.com>
2022-04-26 18:06:12 +0530

    Done

---
+++ Akhil/FileInformation.java
@@ -0,0 +1,18 @@
+package training9;
+
+import java.io.File;
+
+public class FileInformation {
+
+	public static void main(String[] args) {
+		
+		File file = new File("Akhil.txt");
+		
+		System.out.println("File Name: " + file.getName());
+		System.out.println("File path: " + file.getPath());
+		System.out.println("File Absolute Path: " + file.getAbsolutePath());
+		System.out.println("File size: " + file.length());
+
+	}
+
+}

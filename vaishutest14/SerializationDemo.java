0eb4c2734425e3eda1b80f9bbcf7a851ffc62705
Vaish-B <96465233+Vaish-B@users.noreply.github.com>
2022-04-27 15:23:30 +0530

    vaishu/Test14
    added

---
+++ vaishutest14/SerializationDemo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+ class SerializationDemo {
+
+   
+
+
+public static void main(String args[]){    
+  try{    
+  //Creating the object    
+	  SerializationDemo  s = new SerializationDemo();    
+  //Creating stream and writing the object    
+  FileOutputStream fout=new FileOutputStream("f.txt");    
+  ObjectOutputStream out=new ObjectOutputStream(fout);    
+  out.writeObject(s);    
+  out.flush();    
+  //closing the stream    
+  out.close();    
+  System.out.println("success");    
+  }catch(Exception e){System.out.println(e);}    
+ }    
+}    

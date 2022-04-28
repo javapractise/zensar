4802ac618e90d5869992aae5d88c501735b75272
sdmahebub <52583758+sdmahebub@users.noreply.github.com>
2022-04-28 15:55:42 +0530

    Update StudentId.java

--- Mahebub/codingassignment/StudentId.java
+++ Mahebub/codingassignment/StudentId.java
@@ -8,17 +8,26 @@ public class StudentId {
 		Scanner scan = new Scanner(System.in);
 		String s = scan.next();
 		String a = "";
-		String last = "0";
+		String name[] = new String[s.length()];
 
 		for (int i = 0; i < s.length(); i++) {
 			for (int j = i; j < s.length(); j++) {
-				a = s.substring(i,j+1);
-				if(last.charAt(0)<=a.charAt(0)) {
-					last = a;
+				a = s.substring(i, j + 1);
+				name[i] = a;
+			}
+		}
+
+		for (int i = 0; i < name.length - 1; i++) {
+			for (int j = i + 1; j < name.length; j++) {
+				if (name[i].compareTo(name[j]) > 0) {
+					String temp = name[i];
+					name[i] = name[j];
+					name[j] = temp;
 				}
 			}
 		}
-		System.out.println("The last student ID is: "+last);
+
+		System.out.println("The last student ID is: " + name[name.length - 1]);
 	}
 
 }

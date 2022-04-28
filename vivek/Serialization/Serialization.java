003c9f4552606b5246d1a87cd14f0cd835bc34f0
Vivek Ambetkar <v.ambetkar@zensar.com>
2022-04-27 21:43:57 -0700

    Done

---
+++ vivek/Serialization/Serialization.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Student implements java.io.Serializable{
+public String stu_Name;
+public String stu_Addr;
+public int stu_Id;
+public static void main(String [] args) {
+Student s = new Student ();
+s.stu_Name = "Vishal";
+s.stu_Addr "ABC, XYZ"; 
+s.stu_Id 1;
+
+try{
+FileOutputStream fileOut = new FileOutputStream("s.txt");
+
+ObjectOutputStream out = new ObjectOutputStream(fileOut);
+out.writeObject(s);
+out.close();
+fileOu close();
+
+System.out.printf("Object serialized and saved in s.txt");
+}
+ 
+catch (IOException i) { 
+
+i.printStackTrace();
+}
+
+}
+}

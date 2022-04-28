89d127ab20623c29c084a31830fe04d32d795996
jitendra <abc@gmail.com>
2022-04-28 12:40:26 +0530

    JavaTask14_Remaining_Assignment

---
+++ jitendrajava/Assignment14/RunnableDemo.java
@@ -0,0 +1,20 @@
+package javaprogram;
+
+public class RunnableDemo {
+	  public static void main(String[] args) {
+	    System.out.println("From main() method: " + Thread.currentThread().getName());
+	    System.out.println("Creating Runnable Instance");
+	    Runnable runnable = new Runnable() {
+	@Override
+	      public void run() {
+	        System.out.println("From run() method: " + Thread.currentThread().getName());
+	      }
+	    };
+
+	    System.out.println("Creating a Thread Instance");
+	    Thread thread = new Thread(runnable);
+
+	    System.out.println("Launching the thread...");
+	    thread.start();
+	  }
+	}

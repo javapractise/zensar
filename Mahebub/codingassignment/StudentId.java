3a16c0f27da6d2ca46de7d33017d2f1882cae29f
sdmahebub <sdmahebub@gmail.com>
2022-04-28 14:16:56 +0530

    codingAsssignment Completed

---
+++ Mahebub/codingassignment/StudentId.java
@@ -0,0 +1,25 @@
+package codingassignment;
+
+import java.util.Scanner;
+
+public class StudentId {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		String a = "";
+		String last = "0";
+
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i; j < s.length(); j++) {
+				a = s.substring(i,j+1);
+				System.out.println(a);
+				if(last.charAt(0)<=a.charAt(0)) {
+					last = a;
+				}
+			}
+		}
+		System.out.println("The last student ID is: "+last);
+	}
+
+}

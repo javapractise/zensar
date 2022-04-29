5e2b30d88865113f5e7d5e12e7ddc07fa50df9b0
amit <abc@gmail.com>
2022-04-29 15:43:45 +0530

    Done

---
+++ Amit/ReverseList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ReverseList{
+	public static void main(String[] args)
+	{	
+		List<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		a.add(4);
+		System.out.println("List before reverse : "+a);
+		Collections.reverse(a);
+		System.out.println("List after reverse : "+a);
+	}
+}

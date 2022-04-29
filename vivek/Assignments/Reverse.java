ef3a67c88c0c75836040b9ba421c44d29b8cd6d1
Vivek Ambetkar <v.ambetkar@zensar.com>
2022-04-29 15:02:54 +0530

    Done

---
+++ vivek/Assignments/Reverse.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		list.add(27);
+		list.add(41);
+		list.add(78);
+		list.add(57);
+		list.add(15);
+		
+		System.out.println("Original list: "+list);
+		Collections.reverse(list);
+		System.out.println("Reversed list: "+list);
+	}
+
+}

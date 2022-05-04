56ef90ddbefbff50d65d9bd91eaea2891bcd36b4
Shailendra Goad <63011567+sinugaud@users.noreply.github.com>
2022-05-04 16:47:04 +0530

    Update OddEvenLE.java

--- Shailendra/Task2/OddEvenLE.java
+++ Shailendra/Task2/OddEvenLE.java
@@ -5,21 +5,16 @@ interface oddEven {
 	boolean evenOdd(int num);
 }
 
-public class OddEvenLE implements oddEven {
+public class OddEvenLE {
 
 	public static void main(String[] args) {
 
 		oddEven checkOddEven = (a) -> (a % 2 == 0);
 
-		//(a -> a%2 ==0);
+		// (a -> a%2 ==0);
 
 		System.out.println(checkOddEven.evenOdd(2));
 
-	}
-
-	@Override
-	public boolean evenOdd(int num) {
-		return true;
 	}
 
 }

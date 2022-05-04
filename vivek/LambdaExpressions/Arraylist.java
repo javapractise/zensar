71aba8e12f0ad451afcef1d5fcf02a692bdc100d
Vivek Ambetkar <v.ambetkar@zensar.com>
2022-05-04 18:16:46 +0530

    Done

---
+++ vivek/LambdaExpressions/Arraylist.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class IterateArrayList {
+  public static void main(String[] args) {
+    
+    ArrayList<String> languages = new ArrayList<>();
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.print("ArrayList: ");
+    
+    languages.forEach((e) -> {
+      System.out.print(e + ", ");
+    });
+  }
+}

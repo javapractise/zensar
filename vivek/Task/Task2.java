ea154d0b5e11a9d4fdf1d8e7d8105ced92287324
Vivek Ambetkar <v.ambetkar@zensar.com>
2022-05-05 16:43:54 +0530

    Done

---
+++ vivek/Task/Task2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.*;
+public class Stream2 {
+    public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+            myList.stream()
+                  .map(s -> s + "") // Convert integer to String
+                  .filter(s -> s.startsWith("1"))
+                  .forEach(System.out::println);
+    }
+}
---
+++ vivek/Task/Task4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+public class Stream4 {
+    public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+            myList.stream()
+                  .findFirst()
+                  .ifPresent(System.out::println);
+    }
+}

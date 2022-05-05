02b66306f486720917d7dcb9c8b2f353bda8cafc
neeharika <abc@gmail.com>
2022-05-05 14:51:24 +0530

    Done

---
+++ somya/New folder/Javaprogram7.java
@@ -0,0 +1,23 @@
+Given a String, find the first non-repeated character in it using Stream functions?
+
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.Function;
+
+public class Javaprogram7 {
+    public static void main(String args[]) {
+            String input = "Java is Awesome Language";
+
+            Character result = input.chars() // Stream of String       
+                                    .mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s))) // First convert to Character object and then to lowercase         
+                                    .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())) //Store the chars in map with count 
+                                    .entrySet()
+                                    .stream()
+                                    .filter(entry -> entry.getValue() == 1L)
+                                    .map(entry -> entry.getKey())
+                                    .findFirst()
+                                    .get();
+            System.out.println(result);                    
+    }
+}

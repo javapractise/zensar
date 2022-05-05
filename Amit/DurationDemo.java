2fde5a60b6b187db1c6ee1af17a9b4f64aea26d0
amit <abc@gmail.com>
2022-05-05 16:54:08 +0530

    Done

---
+++ Amit/DurationDemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.time.*;
+
+class DurationDemo{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		LocalTime initialTime = LocalTime.of(7, 30, 0);
+		System.out.println("Start Time :"+initialTime);
+
+		System.out.println("Enter time in seconsds to add in time : ");
+		int time = sc.nextInt();
+
+		LocalTime finalTime = initialTime.plus(Duration.ofSeconds(time));
+		System.out.println("End Time :"+finalTime);
+	}
+}

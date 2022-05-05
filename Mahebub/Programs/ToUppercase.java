29feebba13bef7b1a84b9ff0d0e321339562208e
sdmahebub <sdmahebub@gmail.com>
2022-05-05 19:09:38 +0530

    Completed

---
+++ Mahebub/Programs/ToUppercase.java
@@ -0,0 +1,13 @@
+package streamsPrograms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ToUppercase {
+	public static void main(String[] args) {
+		List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
+		String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
+		System.out.println(G7Countries);
+	}
+}

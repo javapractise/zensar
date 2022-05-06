6319b2d826ba65c968a929037d5274ce8da4bb68
hemanth <h.mabbu@zensar.com>
2022-05-06 12:42:50 +0530

    test2

---
+++ hemanth/test2/hemanth6.java
@@ -0,0 +1,19 @@
+package practice;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+public class Thursday {
+
+
+
+public static void main(String[] args) {
+List<String> strList = Arrays.asList("abc", "", "bc", "", "defg", "jklm", "");
+List<String> n = strList.stream() .filter(x -> x.length()> 2) .collect(Collectors.toList());
+System.out.println(n);
+}
+}

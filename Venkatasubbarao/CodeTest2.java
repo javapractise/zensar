75ccfdc01e914a7f234ce9c7cf76a2ef6dd52ac2
Venkata Subbarao Kottakota <v.kottakota@zensar.com>
2022-05-06 21:53:49 +0530

    Done

---
+++ Venkatasubbarao/CodeTest2.java
@@ -0,0 +1,52 @@
+package test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CodeTest2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int nooftoys=sc.nextInt();
+		int[] arr=new int[nooftoys];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		int amount=sc.nextInt();
+		System.out.print(nooftoys(arr,nooftoys,amount));
+
+
+
+	}
+	static int nooftoys(int arr[],int nooftoys,int money) {
+		int sum=0;
+		int count=0;
+		Arrays.sort(arr);
+
+			for(int i=0;i<arr.length;i++)
+			{
+			if(sum+arr[i]<=money)
+			{
+				sum=sum+arr[i];
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
+....................
+sample input1:
+7
+1
+4
+5
+3
+2
+1
+6
+6
+
+sample output1:
+3
